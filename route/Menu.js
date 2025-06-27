68cc7c4fee9a468d9ba39a5b779676e7dea86107
Yesh21102001 <yeswanthkona021@gmail.com>
2025-06-27 12:43:42 +0530

    Saving local changes before pull

--- route/Menu.js
+++ route/Menu.js
@@ -1,71 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   createMenu,
-//   getAllMenu,
-//   getRandomMenu,
-// } = require("../controller/Menu");
-// const upload = require("../Middleware/uploads");
-
-// // const {
-// //   getById,
-// //   getAllMenus,
-// //   createCategory,
-// //   addItemToCategory,
-// //   upload,
-// //   updateCategoryById,
-// //   updateItemById,
-// //   deleteCategoryById,
-// //   createMenu,
-// // } = require("../controller/Menu");
-
-// // router.get("/getMenu", getAllMenus);
-// // router.get("/:id", getById);
-
-// // router.post("/createCategory", createCategory);
-// // router.post("/:categoryId/addItem", upload.single("image"), addItemToCategory);
-
-// // // Updates
-// // router.put("/updateCategory/:id", upload.single("image"), updateCategoryById);
-// // router.put("/updateItem/:categoryId/:itemId", upload.single("image"), updateItemById);
-
-// // // Deletes
-// // router.delete("/deleteCategory/:id", deleteCategoryById);
-// // router.delete("/deleteItem/:categoryId/:itemId", deleteItemById);
-
-
-// const uploadWithErrorHandler = (req, res, next) => {
-//   const multerUpload = upload.fields([
-//     { name: "image", maxCount: 1 },
-//   ]);
-
-//   multerUpload(req, res, function (err) {
-//     if (err) {
-//       req.uploadError = err.message;
-//       console.error("Multer Error:", err.message);
-//     }
-
-//     if (!req.files || !req.files.image || req.files.image.length === 0) {
-//       req.uploadError = req.uploadError || "Image file is required.";
-//       console.log("No image field found in request.");
-//     } else {
-//       console.log("Image field received:", req.files.image[0].originalname);
-//     }
-
-//     next();
-//   });
-// };
-
-
-// router.post("/createMenu", uploadWithErrorHandler, createMenu);
-// router.get("/getAllMenu", getAllMenu);
-// router.get("/getRandomMenu", getRandomMenu);
-
-
-
-// module.exports = router;
-
-
 const express = require("express");
 const router = express.Router();
 
@@ -109,7 +41,6 @@ const uploadWithErrorHandler = (req, res, next) => {
     next();
   });
 };
-
 
 router.get("/getMenu", getAllMenus);
 router.get("/:id", getById);
